e0d68d9158724c4d7588af2b0965007208db861e
Phillip R <philliprosejob@gmail.com>
2022-07-04 20:37:41 -0600

    made sure the confirms for special characters, numbers and uppercase letters are working.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,12 +13,13 @@ var numberArray = [0,1,2,3,4,5,6,7,8,9];
 var specialArray = `!"#$%&'()*+,-./:;<>=?@[]^_|{}~`.split(``);
 // console.log(specialArray);
 
+// used when filling in the rest of the password with a for loop over the combined arrays. 
 var charCount = 0;
 
 // combine all 'true' value characters into this last array to fill password to the correct length 
 var fillerArray = [];
 
-// This will display the password once it is completed and in a string form
+// This will display the password once it is completed and in a string form use arrayName.join('')
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -31,7 +32,7 @@ function passLength(){
   var charLength = prompt('Enter the desired length of your password. Must be between 8 and 128 characters long');
   let number = Number(charLength);
   numCheck(number);
-}
+};
 
 // {{COMPLETED}} makes sure the user input is a number and between 8-128 then calls the special character prompt or resets if input is not valid.
 function numCheck (chars) {
@@ -42,34 +43,62 @@ function numCheck (chars) {
   } else {
     alert(`Your input is invalid, please enter a whole number between 8 and 128`)
     passLength();
-  }
-  
-  
-}
-// console.log(chars + ' this is the return from numCheck');
+  }  
+};
 
-
-
-          // specialPrompt is working correctly 
+// {{COMPLETED}} Once the input is valid, ask if user wants at least 1 special character in their password. Save the answer as true/false. Then move onto the number prompt.
 
 // pick a random element from the special characters array 
 // Used https://stackoverflow.com/a/5915122/19148641 as a resource
 function specialPrompt() {
   console.log(charCount);
-  var specialChars = confirm('do you want at least one special character in your password? Ex:()*+,-./:;<>=?@[]^_|{}'); 
-
+  var specialChars = confirm('do you want at least one special character in your password? Ex:()*+,-./:;<>=?@[]^_|{}');
+  var specialCheck = specialChars;
   if (specialChars) {
     var randomSpecial = specialArray[Math.floor(Math.random()*specialArray.length)];
     fillerArray.push(randomSpecial);
     console.log(fillerArray);
-    // numsPrompt();
+    console.log('this is the output from the confirm! ' + specialChars);
+    numsPrompt(specialCheck);
   } else {
-    console.log('no special characters')
-    // numsPrompt();
+    console.log('no special characters');
+    console.log('this is the output from the confirm? ' + specialChars);
+    numsPrompt(specialCheck);
   }
+  
+};
+ 
 
+
+function numsPrompt(specialCheck){
+  console.log(specialCheck);
+  var numsChars = confirm('Do you want at least one number in your password?');
+  var numsCheck = numsChars;
+  if (numsChars) {
+    var randomNums = numberArray[Math.floor(Math.random()*numberArray.length)];
+    fillerArray.push(randomNums);
+    uppercasePrompt(specialCheck, numsCheck);
+  } else {
+    uppercasePrompt(specialCheck, numsCheck);
+  }
 }
 
+function uppercasePrompt (specialCheck, numsCheck) {
+  var uppercaseChars = confirm('Do you want at least one uppercase letter in your password?');
+  var uppercaseCheck = uppercaseChars;
+  if (uppercaseChars) {
+    var randomUppercase = uppercaseArray[Math.floor(Math.random()*uppercaseArray.length)];
+    fillerArray.unshift(randomUppercase);
+    lowercasePrompt(specialCheck, numsCheck, uppercaseCheck);
+  } else {
+    lowercasePrompt(specialCheck, numsCheck, uppercaseCheck);
+  }
+  console.log('These are the values from the functions so far as T/F ' + specialCheck + ' ' + numsCheck + ' ' + uppercaseCheck);
+};
+
+function lowercasePrompt(specialCheck, numsCheck, uppercaseCheck){
+  console.log('this is the pass so far ! ' + fillerArray);
+}
 
 
 // Add event listener to generate button
